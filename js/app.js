0f4916a35c22f09541201053c9ef5f351b7c9248
Andres Suarez <ansuarez9@gmail.com>
2023-07-23 21:14:20 -0400

    added more to the instructions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// 105 words; last word is corollary
+// 115 words; last word is spaniards
 let words = [
     'hello', 'blue', 'estelle', 'anniversary', 'ring', 'wedding', 'honey', 'john', 'horse', 'mouse', 'house', 
     'train', 'appropriate', 'architecture', 'world', 'moon', 'poetry', 'microsoft', 'apple', 'saving', 'document',
@@ -10,7 +10,8 @@ let words = [
     'hockey', 'basketball', 'elements', 'agriculture', 'keyboard', 'guitar', 'classical', 'theft', 'washington', 'amsterdam',
     'europe', 'saturn', 'submit', 'zoology', 'theoretical', 'spices', 'traitor', 'privately', 'scrabble', 'drafts', 'hamster',
     'hipster', 'finish', 'guessing', 'username', 'password', 'middle', 'large', 'extras', 'smaller', 'stratosphere', 'grounded',
-    'mustache', 'corollary'
+    'mustache', 'corollary', 'ivory', 'scanners', 'scalpers', 'ticketmaster', 'immunodeficiency', 'scientology', 'grove', 'path',
+    'shooters', 'spaniards'
 ];
 
 let playableList = [];
