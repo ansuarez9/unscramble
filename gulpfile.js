e4b31509a01934a3c87cf69a5133328ab24258e8
Andres Suarez <ansuarez9@gmail.com>
2023-08-02 22:04:52 -0400

    added package.json and gulpfile for minification

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const { src, dest } = require('gulp');
+const mininfyJs = require('gulp-uglify');
+const minifyCss = require('gulp-clean-css');
+const minifyHtml = require('gulp-minify-html');
+const rename = require('gulp-rename');
+
+const bundleJs = () => {
+    console.log('bundling js file(s) to /dist/js/ folder');
+    return src('./src/js/**/*.js')
+        .pipe(mininfyJs())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(dest('./dist/js'));
+};
+
+
+const bundleCss = () => {
+    console.log('bundling css file(s) to /dist/styles/ folder');
+    return src('./src/styles/**/*.css')
+        .pipe(minifyCss())
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(dest('./dist/styles'));
+};
+
+const bundleHtml = () => {
+    console.log('bundling html file to /dist/ folder');
+    return src('./src/*.html')
+        .pipe(minifyHtml())
+        .pipe(rename({ extname: '.min.html' }))
+        .pipe(dest('./dist/'));
+};
+
+exports.bundleJs = bundleJs;
+exports.bundleCss = bundleCss;
+exports.bundleHtml = bundleHtml;
