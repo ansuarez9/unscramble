d7828db9badf1db5a0ee15f6a3a1f57c3e894039
Andres Suarez <ansuarez9@gmail.com>
2023-08-02 23:39:50 -0400

    removed .min from minified file names, added localtunnel script

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const bundleJs = () => {
     console.log('bundling js file(s) to /dist/js/ folder');
     return src('./src/js/**/*.js')
         .pipe(mininfyJs())
-        .pipe(rename({ extname: '.min.js' }))
+        // .pipe(rename({ extname: '.min.js' }))
         .pipe(dest('./dist/js'));
 };
 
@@ -17,7 +17,7 @@ const bundleCss = () => {
     console.log('bundling css file(s) to /dist/styles/ folder');
     return src('./src/styles/**/*.css')
         .pipe(minifyCss())
-        .pipe(rename({ extname: '.min.css' }))
+        // .pipe(rename({ extname: '.min.css' }))
         .pipe(dest('./dist/styles'));
 };
 
@@ -25,7 +25,7 @@ const bundleHtml = () => {
     console.log('bundling html file to /dist/ folder');
     return src('./src/*.html')
         .pipe(minifyHtml())
-        .pipe(rename({ extname: '.min.html' }))
+        // .pipe(rename({ extname: '.min.html' }))
         .pipe(dest('./dist/'));
 };
 
